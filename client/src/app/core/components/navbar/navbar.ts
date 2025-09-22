fe65349070ec6c2574e0399e822429f9e8801886
Chancellor Lam <l2.chancellor@gmail.com>
2025-09-22 00:18:02 -0400

    Add basic navigation bar and logo

---
+++ client/src/app/core/components/navbar/navbar.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { NgOptimizedImage } from '@angular/common';
+
+@Component({
+  selector: 'app-navbar',
+  imports: [NgOptimizedImage],
+  templateUrl: './navbar.html',
+  styleUrl: './navbar.css'
+})
+export class Navbar {
+
+}
