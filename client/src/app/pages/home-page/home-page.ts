d9c6be1c41fec03d96549b7e3dfb084f7ebf5473
Chancellor Lam <l2.chancellor@gmail.com>
2025-09-28 03:26:02 -0400

    Implement reactive form for manual creation of scheduling table

--- client/src/app/pages/home-page/home-page.ts
+++ client/src/app/pages/home-page/home-page.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
 import { MatTabGroup, MatTab } from '@angular/material/tabs';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-home-page',
-  imports: [MatTab, MatTabGroup],
+  imports: [MatTab, MatTabGroup, MatFormFieldModule, MatInputModule, ReactiveFormsModule],
   templateUrl: './home-page.html',
-  styleUrl: './home-page.css'
+  styleUrl: './home-page.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class HomePage {
+  matrixDimensions = new FormGroup({
+    numTimeSlots: new FormControl(''), // define matrix width
+    numCandidates: new FormControl(''), // define matrix height
+  });
+
 
 }
